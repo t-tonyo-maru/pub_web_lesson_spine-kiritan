375b3b517dd3b33349f989cf04707aa4e1555750
t-tonyo-maru <49680045+t-tonyo-maru@users.noreply.github.com>
2022-08-24 00:17:13 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// node
+const path = require('path')
+// lib
+const Dotenv = require('dotenv-webpack')
+
+module.exports = (env, args) => {
+  const mode = args.mode || 'development'
+
+  return {
+    mode,
+    target: ['browserslist'],
+    entry: {
+      main: `./src/ts/main.ts`,
+      sample: `./src/ts/sample.ts`
+    },
+    output: {
+      filename: '[name].js',
+      path: `${__dirname}/dist/assets/js`
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: [/node_modules/]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    plugins: [
+      new Dotenv({
+        path: path.resolve(__dirname, `.env.${mode}`)
+      })
+    ]
+  }
+}
